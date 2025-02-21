af15a571eb48be5a00dbc3a679d2f7df183c2db7
DaanSijnja <DaanSijnja@gmail.com>
2025-02-21 20:49:21 +0100

    created basic app structure
    created basic app structure
    - node hiearchy
    - super basic scene loader implimentation
    - only implemented the GUI scene because i dont need a 2d or 3d scene for an application
    
    Sources used
    1. https://www.youtube.com/watch?v=32h8BR0FqdI&list=PLEHvj4yeNfeGiG6ZJXDymk5dYBAjCGiwe&index=4

---
+++ scr/scripts/app_controller.gd
@@ -0,0 +1,21 @@
+class_name AppController extends Node
+
+@export var gui : Control
+
+var current_gui_scene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Global.app_controller = self
+
+func change_gui_scene(new_scene : String, delete : bool = true, keep_running : bool = false) -> void:
+	if current_gui_scene != null:
+		if delete:
+			current_gui_scene.queue_free()
+		elif keep_running:
+			current_gui_scene.visible = false
+		else:
+			gui.remove_child(current_gui_scene)
+	var new = load(new_scene).instantiate()
+	gui.add_child(new)
+	current_gui_scene = new
